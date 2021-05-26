43448e7e314e079a0303177ba3ef2222f9af1541
Shreekz <alanmguadet@gmail.com>
2021-05-25 21:39:53 -0400

    local storage correctly populated on reload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var containerEl = document.querySelector('.container');
 var currentDay = moment().format('dddd MMM Do');
 var toDo = JSON.parse(localStorage.getItem("toDo")) || [];
+var currentHour = moment().format('h');
 $('#currentDay').text(currentDay);
 
 var schedule = {
-    time: [9, 10, 11, 12, 13, 14, 15, 16, 17],
+    time: [8, 9, 10, 11, 12, 13, 14, 15, 16],
     index: [0, 1, 2, 3, 4, 5, 6, 7, 8],
 }
 
@@ -16,52 +17,64 @@ function setHours() {
 
         var hourEl = document.createElement('div');
         hourEl.setAttribute("class", "hour");
-        hourEl.textContent = "PlaceHolder";
+        if (i <= 3){
+            hourEl.textContent = (i + 8) + " AM";
+        } else if (i === 4) {
+            hourEl.textContent = (i + 8) + " PM";
+        } else {
+            hourEl.textContent = (i - 4) + " PM";
+        }
 
-        var textAreaEl = document.createElement('input');
+        var textAreaEl = document.createElement('textarea');
         textAreaEl.setAttribute("class", "description");
-        textAreaEl.setAttribute("type", "textarea");
         textAreaEl.setAttribute("data-index", i);
         textAreaEl.setAttribute("data-hour", schedule.time[i]);
+
+        for (var j = 0; j < toDo.length; j++){
+            if (toDo[j].hour == i) {
+                textAreaEl.value = toDo[j].entry;
+            }
+        }
 
         var buttonEl = document.createElement('button');
         buttonEl.setAttribute("class", "saveBtn");
         buttonEl.setAttribute("data-index", i);
         buttonEl.textContent = "Save";
-        
+
         containerEl.append(block);
         block.append(hourEl);
         block.append(textAreaEl);
         block.append(buttonEl);
-     
+
     }
 }
 
-containerEl.addEventListener("click", function(event){
+containerEl.addEventListener("click", function (event) {
     event.preventDefault();
-
-    if (event.target.matches("button")){
-    var element = event.target;
-    var hourNumber = element.getAttribute("data-index");
-    var entryEl = document.querySelector('input', hourNumber);
-    var savedEntry = {hour: hourNumber, entry: entryEl.value};
-    console.log(entryEl.value);
-    toDo = toDo.concat(savedEntry);
-    console.log(entryEl.value);
-    localStorage.setItem("toDo", JSON.stringify(toDo.concat(savedEntry)));
+    
+    if (event.target.matches("button")) {
+        var element = event.target;
+        var hourNumber = element.getAttribute("data-index");
+        var entryEl = element.previousSibling;
+        var savedEntry = { hour: hourNumber, entry: entryEl.value };
+        console.log(entryEl.value);
+        toDo = toDo.concat(savedEntry);
+        localStorage.setItem("toDo", JSON.stringify(toDo.concat(savedEntry)));
     }
 });
 
-// function timeColors (){
-//     //.isBefore .isAfter .isSame
-//     var timeTest = document.querySelector("data-hour", "hour");
-//     console.log(timeTest.datastate.hour);
+function timeColors (){
+        //.isBefore .isAfter .isSame
+        var timeTest = document.querySelector("data", "hour");
+        console.log(timeTest);
     
+    
+    }
 
-// }
+    function init() {
+        setHours();
+        timeColors();
+        console.log(currentHour);
 
-function init() {
-    setHours();
-    timeColors();
-}
-init();
+    }
+    init();
